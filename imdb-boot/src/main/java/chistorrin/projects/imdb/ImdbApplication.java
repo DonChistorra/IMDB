1f45c1b315c96905ee8d1affc26c57ccd336b83e
DonChistorra <shadowshaoran@gmail.com>
2023-07-30 04:45:25 +0200

    Modificado plugin de verificación de estilos -> maven-checkstyle-plugin:3.1.1 con sun_checks.xml

--- imdb-boot/src/main/java/chistorrin/projects/imdb/ImdbApplication.java
+++ imdb-boot/src/main/java/chistorrin/projects/imdb/ImdbApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ImdbApplication {
 
-	public static void main(String[] args) {
+	protected ImdbApplication() { }
+	public static void main(final String[] args) {
 		SpringApplication.run(ImdbApplication.class, args);
 	}
 
--- imdb-infraestructure/src/main/java/chistorrin/projects/imdb/infraestructure/repository/impl/movie/MovieRepositoryImpl.java
+++ imdb-infraestructure/src/main/java/chistorrin/projects/imdb/infraestructure/repository/impl/movie/MovieRepositoryImpl.java
@@ -1,7 +1,6 @@
 package chistorrin.projects.imdb.infraestructure.repository.impl.movie;
 
 import chistorrin.projects.imdb.domain.repository.MovieRepository;
-import chistorrin.projects.imdb.infraestructure.repository.jpa.esquema.movie.MovieJPARepository;
 
 public class MovieRepositoryImpl implements MovieRepository {
 
