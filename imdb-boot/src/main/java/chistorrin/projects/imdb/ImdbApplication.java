78a00df5f5f63d3ebb9b0b005737ea60ef037754
DonChistorra <shadowshaoran@gmail.com>
2023-07-30 04:58:26 +0200

    Agregada plantilla de excepciones en código

--- imdb-boot/src/main/java/chistorrin/projects/imdb/ImdbApplication.java
+++ imdb-boot/src/main/java/chistorrin/projects/imdb/ImdbApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ImdbApplication {
 
-	protected ImdbApplication() { }
 	public static void main(final String[] args) {
 		SpringApplication.run(ImdbApplication.class, args);
 	}
