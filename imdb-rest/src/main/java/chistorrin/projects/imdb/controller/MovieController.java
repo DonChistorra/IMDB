8055cbbe0754b0ba2e0092550f2ac8cd4663d85d
DonChistorra <105095538+DonChistorra@users.noreply.github.com>
2023-08-02 08:35:12 +0200

    [CH-003] Implementación del endpoint POST /movies
    * Implementación endpoint POST /movies
    
    * Actualización de config para maven-checkstyle-plugin - checkstyle-supressions.xml

--- imdb-rest/src/main/java/chistorrin/projects/imdb/controller/MovieController.java
+++ imdb-rest/src/main/java/chistorrin/projects/imdb/controller/MovieController.java
@@ -1,5 +1,7 @@
 package chistorrin.projects.imdb.controller;
 
+import chistorrin.projects.imdb.domain.entity.Movie;
+import chistorrin.projects.imdb.domain.usecase.CreateMovieUseCase;
 import chistorrin.projects.imdb.domain.usecase.FindAllMoviesUseCase;
 import chistorrin.projects.imdb.mapper.MovieRestMapper;
 import chistorring.projects.imdb.api.MoviesApi;
@@ -17,6 +19,8 @@ public class MovieController implements MoviesApi {
 
 	private final FindAllMoviesUseCase findAllMoviesUseCase;
 
+	private final CreateMovieUseCase createMovieUseCase;
+
 	private final MovieRestMapper movieRestMapper;
 
 	@Override
@@ -27,4 +31,12 @@ public class MovieController implements MoviesApi {
 			HttpStatus.OK);
 	}
 
+	@Override
+	public ResponseEntity<MovieRestDTO> createMovie(final MovieRestDTO movieRestDTO) {
+		Movie domainResponse = this.createMovieUseCase.createMovie(
+			this.movieRestMapper.toDomainEntity(movieRestDTO));
+		return new ResponseEntity<>(
+			this.movieRestMapper.toRestResponse(domainResponse),
+				HttpStatus.OK);
+	}
 }
